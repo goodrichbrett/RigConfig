459d49de6e30db9bea730c99d874245031d24cd8
goodrichbrett <brettgoodrich@outlook.com>
2020-10-11 11:08:59 -0500

    first commit with auth and Part model

---
+++ main_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Part(models.Model):
+    name = models.CharField(max_length=50)
+    component = models.CharField(max_length=20)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"part_id": self.id})
