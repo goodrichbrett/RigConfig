f24070d4ada4c30a17442111373e8833e13d644d
goodrichbrett <brettgoodrich@outlook.com>
2020-10-13 08:37:54 -0500

    add update route and view, add create functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('', views.home, name='home'),
     path('parts/', views.parts_index, name='index'),
+    path('parts/<int:part_id>/', views.parts_detail, name='parts_detail'),
     path('parts/create/', views.PartCreate.as_view(), name='parts_create'),
     path('parts/<int:part_id>/', views.parts_detail, name='detail'),
+    path('parts/<int:part_id>/update/',
+         views.PartUpdate.as_view(), name='parts_update')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -53,3 +53,8 @@ class PartCreate(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class PartUpdate(LoginRequiredMixin, UpdateView):
+    model = Part
+    fiels = '__all__'
